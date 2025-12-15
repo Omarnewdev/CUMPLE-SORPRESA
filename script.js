0913e06689c608f73bf0028cf17b13f34355a9ec
OMAR <erlanomararcevasquez@gmail.com>
2025-12-15 07:18:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// AQUI VAMOS A CREAR EL TEMPORIZADOR
+
+// 👉 CAMBIA ESTA FECHA AL JUEVES CORRECTO
+    const fechaObjetivo = new Date("2025-12-18T00:00:00");
+
+    const bloqueado = document.getElementById("bloqueado");
+    const contenido = document.getElementById("contenido");
+    const contador = document.getElementById("contador");
+
+    function actualizar() {
+        const ahora = new Date();
+        const diferencia = fechaObjetivo - ahora;
+
+        if (diferencia <= 0) {
+            bloqueado.style.display = "none";
+            contenido.style.display = "block";
+            return;
+        }
+
+        const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+        const horas = Math.floor((diferencia / (1000 * 60 * 60)) % 24);
+        const minutos = Math.floor((diferencia / (1000 * 60)) % 60);
+        const segundos = Math.floor((diferencia / 1000) % 60);
+
+        contador.innerHTML =
+            `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+    }
+
+    setInterval(actualizar, 1000);
+    actualizar();
